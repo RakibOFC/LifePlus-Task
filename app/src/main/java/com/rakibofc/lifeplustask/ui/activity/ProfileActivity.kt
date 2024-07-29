1c6a7f6ea01383f3250cb4005f7bb76aa0c2dbec
Rakibul Islam <rakibull2000@gmail.com>
2024-07-29 23:21:48 +0600

    ui design for activity_profile.xml

--- app/src/main/java/com/rakibofc/lifeplustask/ui/activity/ProfileActivity.kt
+++ app/src/main/java/com/rakibofc/lifeplustask/ui/activity/ProfileActivity.kt
@@ -10,12 +10,8 @@ import com.rakibofc.lifeplustask.R
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
