7590480dc5f2845251cd17acab8aac9667d052b4
Rakibul Islam <rakibull2000@gmail.com>
2024-07-30 20:28:22 +0600

    Implement show data in DetailsActivity

--- app/src/main/java/com/rakibofc/lifeplustask/ui/activity/DetailsActivity.kt
+++ app/src/main/java/com/rakibofc/lifeplustask/ui/activity/DetailsActivity.kt
@@ -1,20 +1,21 @@
 package com.rakibofc.lifeplustask.ui.activity
 
 import android.os.Bundle
+import android.text.Html
+import android.text.Spanned
 import android.util.Log
 import android.view.View
-import android.view.WindowInsets
-import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.google.gson.Gson
+import com.rakibofc.lifeplustask.R
 import com.rakibofc.lifeplustask.data.remote.Show
 import com.rakibofc.lifeplustask.databinding.ActivityDetailsBinding
 import com.rakibofc.lifeplustask.util.UiState
 import com.rakibofc.lifeplustask.viewmodel.MainViewModel
+import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
-
 
 @AndroidEntryPoint
 class DetailsActivity : BaseActivity() {
@@ -26,11 +27,11 @@ class DetailsActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
 
         // Make the status bar translucent
-        val window = window
+        /*val window = window
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        )
+        )*/
 
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -67,6 +68,7 @@ class DetailsActivity : BaseActivity() {
 
             is UiState.Success -> {
                 Log.e("TAG", "Success: ${Gson().toJson(showUiState.data)}")
+                handleShowDetailsSuccess(showUiState.data)
             }
         }
     }
@@ -77,5 +79,55 @@ class DetailsActivity : BaseActivity() {
         lifecycleScope.launch {
             viewModel.setShowDetails(showDetails)
         }
+    }
+
+    private fun handleShowDetailsSuccess(show: Show) {
+
+        val genres = show.genres.joinToString(", ")
+        val schedule =
+            getStringFormat(listOf(show.schedule.days.joinToString(", "), show.schedule.time))
+        // Load show image
+        loadShowThumbnail(show.image?.medium)
+
+        // Set show details
+        binding.tvShowName.text = show.name
+        binding.tvLangGenTime.text =
+            getStringFormat(
+                listOf(
+                    show.language, genres.ifBlank { "" },
+                    if (show.runtime != null) "${show.runtime} mins" else ""
+                )
+            )
+        if (schedule.isNotBlank())
+            binding.tvShowSchedule.text = schedule
+        else
+            binding.tvShowSchedule.visibility = View.GONE
+
+        val summary = show.summary ?: ""
+        val spannedSummary: Spanned = Html.fromHtml(summary, Html.FROM_HTML_MODE_COMPACT)
+        binding.tvShowSummary.text = spannedSummary
+    }
+
+    private fun getStringFormat(strings: List<String>): String {
+        var langGenTime = ""
+        strings.indices.forEach {
+            val string = strings[it]
+            if (string.isNotBlank()) {
+                langGenTime += string
+                if (it < strings.size - 1) {
+                    langGenTime += " • "
+                }
+            }
+        }
+
+        return langGenTime
+    }
+
+    private fun loadShowThumbnail(imageUrl: String?) {
+        Picasso.get()
+            .load(imageUrl)
+            .placeholder(R.drawable.placeholder)
+            .error(R.drawable.placeholder)
+            .into(binding.ivShowImage)
     }
 }
